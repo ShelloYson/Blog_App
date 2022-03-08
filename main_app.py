311305a7c292513b07b5d2b782563655f8d88259
ShelloYson <80486536+ShelloYson@users.noreply.github.com>
2022-03-09 12:19:07 +1300

    Comments
    Added comments to all in the main app code

--- main_app.py
+++ main_app.py
@@ -1,9 +1,12 @@
+# This is a blog app that allows users to register and post blogs
 from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5e4c4a1ab3d094fa267709999c1a2fd2'
+
+# Lists of post dictionaries
 
 posts = [
     
@@ -30,24 +33,31 @@ posts = [
     
 ]
 
+# Routes
+
+# Route for homepage
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+# Route for about page
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
+# Route for registration page
 @app.route("/register")
 def register():
     form = RegistrationForm()
     return render_template('register.html', title='Register', form=form)
 
+# Route for login page
 @app.route("/login")
 def login():
     form = LoginForm()
     return render_template('login.html', title='Login', form=form)
 
+# Allows main app to run the web server
 if __name__=='__main__':
     app.run(debug=True)
