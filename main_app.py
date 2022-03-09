e057f52c903e7803aaa0f856d85eb4fd1f9ebc15
ShelloYson <80486536+ShelloYson@users.noreply.github.com>
2022-03-09 19:33:36 +1300

    Created user database class

--- main_app.py
+++ main_app.py
@@ -1,10 +1,23 @@
 # This is a blog app that allows users to register and post blogs
+from crypt import methods
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '5e4c4a1ab3d094fa267709999c1a2fd2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 # Lists of post dictionaries
 
@@ -56,9 +69,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 # Route for login page
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Logged Unsuccessful. Please check your username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 # Allows main app to run the web server
