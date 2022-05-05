cf39936e5aa3aefe137281ef7003d5f1e0f1b6ac
ShelloYson <80486536+ShelloYson@users.noreply.github.com>
2022-05-06 10:54:10 +1200

    New Post
    Added that users can post stuff.

--- main_app/routes.py
+++ main_app/routes.py
@@ -1,8 +1,9 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from main_app import app, db, bcrypt
-from main_app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from main_app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from main_app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -87,7 +88,10 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
     
     return picture_fn
 
@@ -110,3 +114,13 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+# Route for new posts
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Post has been created', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
